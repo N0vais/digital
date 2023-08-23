f82e0e7b3aef6454af4b05875f7cbc26e5f68ce9
Daniel Novais <danlicesol@gmail.com>
2023-08-22 23:44:53 -0300

    reparando _blog

---
+++ http.js
@@ -0,0 +1,21 @@
+Requisição HTTP
+
+     URL: https://pokeapi.co/api/v2/pokemon?offset=10&lomit=10
+          ${IP}/${path = caminho de identificação do recurso}
+
+     Request Method: GET | POST | PUT | DELETE
+
+     Request Headers
+     content-type: applocation/JSON
+          ReportBody{
+               "name": "Test"
+          }
+
+          accept-language:
+          pt-br;q=0.1
+          pt;q=0.9
+          en-Us;q=0.8
+          en;q=0.7
+          globalThis;q=0.6
+
+          Response Headers
